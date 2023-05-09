a223cb3194871f68111ccbfbde74887f0b79df46
Richard Koeppe <richard.koeppe@outlook.de>
2023-05-09 17:19:48 +0000

    Example for versioning. Publishing two messages.

--- scripts/test.py
+++ scripts/test.py
@@ -7,6 +7,8 @@ class TestPackage:
     def __init__(self):
         # Create publisher
         self.ackermann_publisher = rospy.Publisher('/ackermann_cmd', AckermannDriveStamped, queue_size=1) 
+        self.ackermann_publisher_2 = rospy.Publisher('/ackermann_cmd_2', AckermannDriveStamped, queue_size=1) 
+
 
     def do_stuff(self):
         # Generate artificial data
@@ -17,6 +19,14 @@ class TestPackage:
         # publish the data
         self.ackermann_publisher.publish(ackermann_message)
         
+        # Generate artificial data
+        ackermann_message = AckermannDriveStamped()
+        ackermann_message.header.stamp = rospy.Time.now()
+        ackermann_message.drive.steering_angle = 1
+        ackermann_message.drive.speed = 2
+        # publish the data
+        self.ackermann_publisher_2.publish(ackermann_message)
+        
 if __name__ == '__main__':
     rospy.init_node('test_package', anonymous=True)
     
@@ -25,6 +35,6 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         try:
             test_package.do_stuff()
-            rospy.sleep(1)
+            rospy.sleep(0.5)
         except KeyboardInterrupt:
             print("Interrupted. Shutting down...")
